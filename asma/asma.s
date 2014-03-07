a074c350b373e281cd29b13f54a0b0b8bcc8a2be
Christian Hotz-Behofsits <chris.hotz.behofsits@gmail.com>
2014-03-07 12:42:20 +0100

    comments added

--- asma/asma.s
+++ asma/asma.s
@@ -4,37 +4,38 @@
 _asma:                                  ## @asma
 	.cfi_startproc
 ## BB#0:
-	pushq	%rbp
+	pushq	%rbp						# frame pointer backup
 Ltmp2:
 	.cfi_def_cfa_offset 16
 Ltmp3:
 	.cfi_offset %rbp, -16
-	movq	%rsp, %rbp
+	movq	%rsp, %rbp					# set new frame pointer
 
 Ltmp4:
 	.cfi_def_cfa_register %rbp			## set new CFA
 
-## 16 byte alignment. with nop (no operation) as padding
-#
-# http://stackoverflow.com/questions/14544617/what-does-the-align-directive-do
-# http://stackoverflow.com/questions/18413107/align-directive-proper-usage-with-align5-and-0x90
-##
+	## 16 byte alignment. with nop (no operation) as padding
+	#
+	# http://stackoverflow.com/questions/14544617/what-does-the-align-directive-do
+	# http://stackoverflow.com/questions/18413107/align-directive-proper-usage-with-align5-and-0x90
+	##
 	.align	4, 0x90						
 
-## copy rsi to xmm0
-movdqa (%rsi), %xmm0
+	## 1. copy rsi to xmm0
+	movdqa (%rsi), %xmm0
 
-## copy rdi to xmm1
-movdqa (%rdi), %xmm1
+	## 2. copy rdi to xmm1
+	movdqa (%rdi), %xmm1
 
-## compare bytewise
-pminub %xmm0, %xmm1
+	## 3. compare bytewise
+	pminub %xmm0, %xmm1
 
-## first operand xmm0 is result of smallest values (return it)
-movdqa %xmm0, (%rdx)
+	## 4. first operand xmm0 is result of smallest values (return it)
+	movdqa %xmm0, (%rdx)
 
 LBB0_3:
-	popq	%rbp
+	xorl %eax, %eax  					# zero function return
+	popq	%rbp						# pop frame pointer
 	ret
 	.cfi_endproc
 
