57a68d30131d1e52a8179264a7172a5c16e6b8f1
Christian Hotz-Behofsits <chris.hotz.behofsits@gmail.com>
2014-03-07 12:00:32 +0100

    removed loop

--- asma/asma.s
+++ asma/asma.s
@@ -10,8 +10,30 @@ Ltmp2:
 Ltmp3:
 	.cfi_offset %rbp, -16
 	movq	%rsp, %rbp
+
 Ltmp4:
-	.cfi_def_cfa_register %rbp
+	.cfi_def_cfa_register %rbp			## set new CFA
+
+## 16 byte alignment. with nop (no operation) as padding
+#
+# http://stackoverflow.com/questions/14544617/what-does-the-align-directive-do
+# http://stackoverflow.com/questions/18413107/align-directive-proper-usage-with-align5-and-0x90
+##
+	.align	4, 0x90						
+
+## copy rsi to xmm0
+movdqa %rsi, %xmm0
+
+## copy rdi to xmm1
+movdqa %rdi, %xmm1
+
+## compare bytewise
+pminub %xmm0, %xmm1
+
+## first operand xmm0 is result of smallest values (return it)
+movdqa %xmm0, %rdx
+
+LBB0_3:
 	popq	%rbp
 	ret
 	.cfi_endproc
