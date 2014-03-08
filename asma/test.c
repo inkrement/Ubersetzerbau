8f3d0618b70e8ac1410f4d5763e3dce6086ccbc7
Christian Hotz-Behofsits <chris.hotz.behofsits@gmail.com>
2014-03-08 14:29:58 +0100

    minor bugfixes: prevent warnings

--- asma/test.c
+++ asma/test.c
@@ -12,9 +12,17 @@ int main(){
 	unsigned char *second = (unsigned char*)malloc(STR_LEN*sizeof(unsigned char));
 	unsigned char *result = (unsigned char*)malloc(STR_LEN*sizeof(unsigned char));
 
-	strcpy(first, "abcdefghikl");
-	strcpy(second, "haha");
-	strcpy(result, "result");
+	const char *str_operand1= "abcdefghijklm";
+	const char *str_operand2= "haha";
+	const char *str_result= "abcdefghijklm";
+	
+	//strcpy(first, "abcdefghikl");
+	//strcpy(second, "haha");
+	//strcpy(result, "result");
+
+	memcpy(first, str_operand1, strlen(str_operand1));
+	memcpy(second, str_operand2, strlen(str_operand2));
+	memcpy(result, str_result, strlen(str_result));
 
 	asma(first, second, result);
 
