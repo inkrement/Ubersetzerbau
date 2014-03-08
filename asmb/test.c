b4dca1f2bfc4ed23ebc5f6030e51a0b5785781e6
Christian Hotz-Behofsits <chris.hotz.behofsits@gmail.com>
2014-03-08 18:06:07 +0100

    asmb added

---
+++ asmb/test.c
@@ -0,0 +1,33 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+
+#define STR_LEN 16
+
+extern void asmb(unsigned char *s, unsigned char *t, unsigned char *u);
+
+int main(){
+
+	unsigned char *first = (unsigned char*) malloc(STR_LEN * sizeof(unsigned char));
+	unsigned char *second = (unsigned char*)malloc(STR_LEN*sizeof(unsigned char));
+	unsigned char *result = (unsigned char*)malloc(STR_LEN*sizeof(unsigned char));
+
+	const char *str_operand1= "abcdefghijklm";
+	const char *str_operand2= "haha";
+	const char *str_result= "abcdefghijklm";
+	
+	//strcpy(first, "abcdefghikl");
+	//strcpy(second, "haha");
+	//strcpy(result, "result");
+
+	memcpy(first, str_operand1, strlen(str_operand1));
+	memcpy(second, str_operand2, strlen(str_operand2));
+	memcpy(result, str_result, strlen(str_result));
+
+	asmb(first, second, result);
+
+	fwrite(result, sizeof(unsigned char), STR_LEN, stdout);
+	//printf("%.*s\n", STR_LEN, result);
+
+	return 0;
+}
