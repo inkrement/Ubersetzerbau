f0d83e2ed3fc42d5740236cbabc8deff1462d5ed
Christian Hotz-Behofsits <chris.hotz.behofsits@gmail.com>
2014-03-12 11:02:29 +0100

    prototype

--- asma/asma.s
+++ asma/asma.s
@@ -17,10 +17,22 @@ Ltmp4:
 	## 2. copy rdi to xmm1
 	movdqa (%rdi), %xmm1
 
-	## 3. compare bytewise
-	pmaxub %xmm0, %xmm1
+	## 3. create mask
+	movdqa $0xFFFFFFFF , %xmm2
 
-	## 4. first operand xmm0 is result of smallest values (return it)
+	## 4. s xmm0 invertieren
+    andps %xmm0, %xmm2
+
+	## 5. t xmm1 invertieren
+	andps %xmm1, %xmm2
+
+	## 6. take minimal 
+	pminub %xmm0, %xmm1
+
+	## 7. invert result
+	andps %xmm1, %xmm2
+
+	## 8. first operand xmm0 is result of smallest values (return it)
 	movdqa %xmm1, (%rdx)
 
 LBB0_3:
