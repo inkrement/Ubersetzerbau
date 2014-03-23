479ab8b0e733e89a4f2d59d6f10799a7b707754b
Dominik Pichler <dpichler90@gmail.com>
2014-03-23 13:19:41 +0100

    some fix

--- asmb/asmb.s
+++ asmb/asmb.s
@@ -13,27 +13,27 @@
 
 # STEP 1 reset zählregister
 
-xor %eax, %eax
+xor %rax, %rax
 
 _asmb:
 
 loop:
 	# STEP 2 copy param byte-wise
+	movb (%rdi, %rax, 1), %r8b
+	movb (%rsi, %rax, 1), %r9b
+	movb (%rdx, %rax, 1), %r10b
 
-	movb (%edi, %eax, 1), %r8b
-	movb (%edi, %eax, 1), %r9b
-	movb (%edx, %eax, 1), %r10b
-
-	PINSRB $0x01, %r8d, %xmm1
-	PINSRB $0x01, %r9d, %xmm2
+	# 1 byte insertn und extrahieren
+	PINSRB $0x00, %r8d, %xmm1
+	PINSRB $0x00, %r9d, %xmm2
 
 	# Step 3: calculate and write to result
 	pminub %xmm1, %xmm2
 
-	PEXTRB $0x01 , %xmm2, (%edi, %eax,1)
+	PEXTRB $0x00 , %xmm2, (%rdi, %rax,1)
 
 	# Step 3: increment
-	incl %eax
+	incl %rax
 
 	# Step 4 conditional loop
 
