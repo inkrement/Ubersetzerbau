738010eaba2a77466a738454e2003f606bb9c996
Christian Hotz-Behofsits <chris.hotz.behofsits@gmail.com>
2014-03-23 14:35:03 +0100

    bugfix

--- asmb/asmb.s
+++ asmb/asmb.s
@@ -1,6 +1,6 @@
 	.text
-	.globl asmb
-	.type asmb, @function
+	.globl _asmb
+#	.type asmb, @function
 
 ####
 # rdi erstes argument
@@ -13,19 +13,20 @@
 
 # STEP 1 reset zählregister
 
-xor %rax, %rax
+xor %rbx, %rbx
 
-asmb:
+_asmb:
 
 loop:
-
 	cmp $0, %r10b
+
 	je exit
 
+
 	# STEP 2 copy param byte-wise
-	movb (%rdi, %rax, 1), %r8b
-	movb (%rsi, %rax, 1), %r9b
-	movb (%rdx, %rax, 1), %r10b
+	movb (%rdi, %rbx), %r8b
+	movb (%rsi, %rbx), %r9b
+	movb (%rdx, %rbx), %r10b
 
 	# 1 byte insertn und extrahieren
 	PINSRB $0x00, %r8d, %xmm1
@@ -34,10 +35,10 @@ loop:
 	# Step 3: calculate and write to result
 	pminub %xmm1, %xmm2
 
-	PEXTRB $0x00 , %xmm2, (%rdi, %rax,1)
+	PEXTRB $0x00 , %xmm2, (%rdx, %rbx)
 
 	# Step 3: increment
-	inc %rax
+	inc %rbx
 
 	jmp loop
 
