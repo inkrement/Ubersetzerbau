dc39aac3801f469d09cde55e29f0f45606d58114
Christian Hotz-Behofsits <chris.hotz.behofsits@gmail.com>
2014-03-23 16:06:01 +0100

    beta

--- asmb/asmb.s
+++ asmb/asmb.s
@@ -1,25 +1,32 @@
 	.text
-	.globl asmb
-	.type asmb, @function
+	.globl _asmb
+#	.type asmb, @function
 
-asmb:
+_asmb:
 
 xor %rcx, %rcx
+pxor %xmm2, %xmm2
+pxor %xmm3, %xmm3 # mask
 
 loop:
 
 	movb (%rdi, %rcx), %r8b
 	movb (%rsi, %rcx), %r9b
 	
-	cmp $0, %r8b
-	je exit
-	cmp $0, %r9b
-	je exit
+	#cmp $0, %r8b
+	#je exit
+	#cmp $0, %r9b
+	#je exit
 
 	PINSRB $0x00, %r8d, %xmm1
 	PINSRB $0x00, %r9d, %xmm2
 
 	pminub %xmm1, %xmm2
+
+	CMPEQPS %xmm2, %xmm3
+	je exit
+
+
 	PEXTRB $0x00 , %xmm2, (%rdx, %rcx)
 
 	inc %rcx
